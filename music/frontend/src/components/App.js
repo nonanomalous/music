9bcd6acbb93958db9d44b214bb427aab42f7cabb
Brett M <hxr@outlook.com>
2021-01-25 06:35:11 -0700

    First commit

--- music/frontend/src/components/App.js
+++ music/frontend/src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Homepage from "./Homepage";
 
 export class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      key: "value",
+    };
   }
 
   render() {
-    return <h1>Testing react</h1>;
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
   }
 }
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Brett" />, appDiv);
--- music/frontend/urls.py
+++ music/frontend/urls.py
@@ -3,4 +3,6 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('join', index, name='join'),
+    path('create', index, name='create'),
 ]
